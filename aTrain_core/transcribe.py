aa7908d0887c0199f322aa3bc4602976fbbd8c3b
ArminHaberl <haberlarmin@gmx.at>
2024-10-01 15:23:33 +0200

    revert function name

--- aTrain_core/transcribe.py
+++ aTrain_core/transcribe.py
@@ -105,7 +105,7 @@ def transcription_with_progress_bar(transcription_segments, info, GUI: EventSend
     return transcription_segments_new
 
 
-def prepare_metadata_creation(language, num_speakers, device, file_id, audio_file):
+def _prepare_metadata_creation(language, num_speakers, device, file_id, audio_file):
     """Preprocessing steps for the metadata creation and further use."""
     language = None if language == "auto-detect" else language
     min_speakers = max_speakers = (
@@ -301,7 +301,7 @@ def transcribe(
     write_logfile("Directory created", file_id)
 
     audio_array, audio_duration, device, min_speakers, max_speakers, language = (
-        prepare_metadata_creation(language, num_speakers, device, file_id, audio_file)
+        _prepare_metadata_creation(language, num_speakers, device, file_id, audio_file)
     )
     create_metadata(
         file_id,
