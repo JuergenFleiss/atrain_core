42739efc63ecb23e27bff73bd23c4acfc6030071
ArminHaberl <haberlarmin@gmx.at>
2024-10-02 13:40:09 +0200

    change required model to large-v3-turbo

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -9,4 +9,4 @@ METADATA_FILENAME = "metadata.txt"
 LOG_FILENAME = "log.txt"
 TIMESTAMP_FORMAT = "%Y-%m-%d %H-%M-%S"
 SAMPLING_RATE = 16000
-REQUIRED_MODELS = ["diarize", "large-v3"]
+REQUIRED_MODELS = ["diarize", "large-v3-turbo"]
