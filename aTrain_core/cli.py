1b3b55ee32f7a5dc85096101ab4903b85d0643ae
andreaforster <97340144+chicadelica@users.noreply.github.com>
2024-12-09 11:46:27 +0100

    add float32 to compute types

--- aTrain_core/cli.py
+++ aTrain_core/cli.py
@@ -78,8 +78,8 @@ def cli():
     parser_transcribe.add_argument(
         "--compute_type",
         default="int8",
-        choices=["float16", "int8"],
-        help="Compute type (float16/int8)",
+        choices=["float16", "int8", "float32"],
+        help="Compute type (int8/float16/float32)",
     )
 
     args = parser.parse_args()
