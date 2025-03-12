53414a5b5a915cff950e2f45ce39247473bae937
ArminHaberl <haberlarmin@gmx.at>
2025-03-12 16:56:47 +0100

    change REQUIRED_MODELS_DIR if aTrain is imported

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -1,4 +1,5 @@
 import os
+import sys
 from importlib.resources import files
 
 
@@ -6,10 +7,11 @@ USER_DIR = os.path.expanduser("~")
 DOCUMENTS_DIR = os.path.join(USER_DIR, "Documents")
 ATRAIN_DIR = os.path.join(DOCUMENTS_DIR, "aTrain")
 MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
-REQUIRED_MODELS_DIR = files("aTrain") / "required_models"
+REQUIRED_MODELS_DIR = files("aTrain") / "required_models" if "aTrain" in sys.modules else MODELS_DIR
 REQUIRED_MODELS = ["diarize", "large-v3-turbo"]
 TRANSCRIPT_DIR = os.path.join(ATRAIN_DIR, "transcriptions")
 METADATA_FILENAME = "metadata.txt"
 LOG_FILENAME = "log.txt"
 TIMESTAMP_FORMAT = "%Y-%m-%d %H-%M-%S"
 SAMPLING_RATE = 16000
+
