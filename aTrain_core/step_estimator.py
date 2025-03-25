08a6a71ef751fabfedf07ad1bd8648d9f749905d
ArminHaberl <haberlarmin@gmx.at>
2025-03-25 19:38:15 +0100

    remove unused code

--- aTrain_core/step_estimator.py
+++ aTrain_core/step_estimator.py
@@ -1,17 +1,4 @@
-import json
 import math
-from importlib.resources import files
-
-
-def load_model_config_file():
-    """Loads the model configuration file.
-    Function defined again to avoid circular import error"""
-
-    # only load large v3
-    models_config_path = str(files("aTrain_core.data").joinpath("models.json"))
-    with open(models_config_path, "r") as models_config_file:
-        models_config = json.load(models_config_file)
-    return models_config
 
 
 def calculate_steps(audio_duration):
@@ -48,19 +35,3 @@ def predict_embedding_steps(length):
     b = 6779072611703841 / 36028797018963968
     c = -3802017452395983 / 18014398509481984
     return a * length**2 + b * length + c
-
-
-def get_total_model_download_steps(model_name):
-    """A function that finds the total download chunks (steps) for a given model.
-    The metadata has been pre-calculated by downloading the models"""
-
-    models_config = load_model_config_file()
-    model_info = models_config[model_name]
-
-    if model_name in models_config:
-        total_chunks = model_info["chunks_total"]
-
-    else:
-        total_chunks = None
-
-    return total_chunks
