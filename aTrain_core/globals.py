ea8c89cd89c2470cdbe36a04b4db85a5e5a03843
Jürgen Fleiß <118339672+JuergenFleiss@users.noreply.github.com>
2025-04-02 08:50:11 +0200

    testing snap dirs

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -3,12 +3,12 @@ import sys
 from importlib.resources import files
 
 
-USER_DIR = os.path.expanduser("~")
+USER_DIR = os.environ['SNAP_USER_DATA']
 DOCUMENTS_DIR = os.path.join(USER_DIR, "Documents")
 ATRAIN_DIR = os.path.join(DOCUMENTS_DIR, "aTrain")
 MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
-REQUIRED_MODELS_DIR = files("aTrain") / "required_models" if "aTrain" in sys.modules else MODELS_DIR
-REQUIRED_MODELS = ["diarize", "large-v3-turbo"]
+REQUIRED_MODELS_DIR = MODELS_DIR
+REQUIRED_MODELS = []
 TRANSCRIPT_DIR = os.path.join(ATRAIN_DIR, "transcriptions")
 METADATA_FILENAME = "metadata.txt"
 LOG_FILENAME = "log.txt"
