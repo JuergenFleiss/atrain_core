55a4c342790b1333e16669bd228fb31652124689
JuergenFleiss <juergen.fleiss@uni-graz.at>
2025-04-03 12:38:10 +0200

    change globals.py paths

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -4,10 +4,10 @@ from importlib.resources import files
 
 
 USER_DIR = os.environ['SNAP_USER_DATA']
-DOCUMENTS_DIR = os.path.join(USER_DIR, "Documents")
+DOCUMENTS_DIR = USER_DIR
 ATRAIN_DIR = os.path.join(DOCUMENTS_DIR, "aTrain")
 MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
-REQUIRED_MODELS_DIR = MODELS_DIR
+REQUIRED_MODELS_DIR = os.environ['SNAP_DATA']
 REQUIRED_MODELS = ["diarize", "large-v3-turbo"]
 TRANSCRIPT_DIR = os.path.join(ATRAIN_DIR, "transcriptions")
 METADATA_FILENAME = "metadata.txt"
