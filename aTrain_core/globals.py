ef4ec073059c3e816bbaf2683479b236dce2f79a
ArminHaberl <haberlarmin@gmx.at>
2025-12-18 15:28:46 +0100

    use cross platform library to find documents folder of users

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -1,16 +1,14 @@
-import os
 from importlib.resources import files
 from importlib.util import find_spec
+from platformdirs import user_documents_path
 
-USER_DIR = os.path.expanduser("~")
-DOCUMENTS_DIR = os.path.join(USER_DIR, "Documents")
-ATRAIN_DIR = os.path.join(DOCUMENTS_DIR, "aTrain")
-MODELS_DIR = os.path.join(ATRAIN_DIR, "models")
+ATRAIN_DIR = user_documents_path() / "aTrain"
+MODELS_DIR = ATRAIN_DIR / "models"
 REQUIRED_MODELS_DIR = (
     files("aTrain") / "required_models" if find_spec("aTrain") else MODELS_DIR
 )
 REQUIRED_MODELS = ["diarize", "large-v3-turbo"]
-TRANSCRIPT_DIR = os.path.join(ATRAIN_DIR, "transcriptions")
+TRANSCRIPT_DIR = ATRAIN_DIR / "transcriptions"
 METADATA_FILENAME = "metadata.txt"
 LOG_FILENAME = "log.txt"
 TIMESTAMP_FORMAT = "%Y-%m-%d %H-%M-%S"
