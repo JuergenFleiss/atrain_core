40c4797a96ab1fe8cebf397f86366352076d41b8
ArminHaberl <haberlarmin@gmx.at>
2026-01-14 10:47:16 +0100

    simplify typing of paths

--- aTrain_core/globals.py
+++ aTrain_core/globals.py
@@ -1,12 +1,17 @@
 from importlib.resources import files
 from importlib.util import find_spec
+from pathlib import Path
+from typing import cast
+
 from platformdirs import user_documents_path
 
 ATRAIN_DIR = user_documents_path() / "aTrain"
 MODELS_DIR = ATRAIN_DIR / "models"
-REQUIRED_MODELS_DIR = (
-    files("aTrain") / "required_models" if find_spec("aTrain") else MODELS_DIR
-)
+if find_spec("aTrain"):
+    REQUIRED_MODELS_DIR = cast(Path, files("aTrain") / "required_models")
+else:
+    REQUIRED_MODELS_DIR = MODELS_DIR
+print(REQUIRED_MODELS_DIR, type(REQUIRED_MODELS_DIR))
 REQUIRED_MODELS = ["speaker-detection", "large-v3-turbo"]
 TRANSCRIPT_DIR = ATRAIN_DIR / "transcriptions"
 METADATA_FILENAME = "metadata.txt"
